6be7ad516659b764cee3368beffd7b9f7dfc5efc
Georgi Tanev <georgi.tanev86@gmail.com>
2021-03-25 14:51:46 +0200

    new oracle sql code

---
+++ oracle/testov_sql.sql
@@ -0,0 +1,7 @@
+﻿/* selecting 3 column in case if phone number like 515% - replace it with 'not working anymore'
+SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME,
+    (case 
+    when PHONE_NUMBER like '515%' then  PHONE_NUMBER else 'not working anymore' END) phone_number
+    FROM employees
+*/
+
