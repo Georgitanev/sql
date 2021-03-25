77bacb2692ac65d15dc7634d14d1b7a3bdef5d8b
Georgi Tanev <georgi.tanev86@gmail.com>
2021-03-25 15:02:06 +0200

    case added with salary ranges checks

--- oracle/testov_sql.sql
+++ oracle/testov_sql.sql
@@ -5,3 +5,20 @@ SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME,
     FROM employees
 */
 
+/* select with case -> check salary and return different strings. Formatted code
+SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME,
+	(CASE 
+		when PHONE_NUMBER like '515%' then PHONE_NUMBER else 'not working anymore' 
+	 END)                                                               phone_number,
+														
+	CASE 
+		when SALARY < 1000 					   then 'low salary'
+		when SALARY >= 1000 and SALARY <= 5000 then 'medium salary'
+											   else 'big salary' 
+	END                                                                 salary_level
+FROM employees
+*/
+	
+	
+	
+	
