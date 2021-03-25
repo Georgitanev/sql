c735313a7b3ec0cad747cfda0e2d15ac5f4a9a2f
Georgi Tanev <georgi.tanev86@gmail.com>
2021-03-25 15:29:45 +0200

    basic join and Join with Alias 'e' and 'd' . Added department.

--- oracle/testov_sql.sql
+++ oracle/testov_sql.sql
@@ -71,3 +71,47 @@ SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME,
 									'other_color')  "COLORS" 
     FROM employees
     */
+	
+/* Basic JOIN
+select *
+from employees e JOIN DEPARTMENTS d
+on (e.department_id = d.department_id);
+*/
+
+
+/* Join with Alias 'e' and 'd' . Added department. 
+SELECT e.EMPLOYEE_ID, e.FIRST_NAME, e.LAST_NAME, d.department_name,
+    (CASE 
+        when e.PHONE_NUMBER   like '515%'         then e.PHONE_NUMBER else 'not working anymore'
+     END)                                                               phone_number    ,
+                                                                        e.SALARY        ,     
+    CASE 
+        when e.SALARY <  1000                     then 'low salary'
+        when e.SALARY >= 1000 and e.SALARY < 3000 then 'medium salary'
+        when e.SALARY >= 3000 and e.SALARY < 6000 then 'big salary'
+        when e.SALARY >= 6000 and e.SALARY < 8000 then 'huge salary'
+        else                                           'top salary' 
+    END                                                                 salary_level    ,
+    CASE
+        when d.DEPARTMENT_ID <  60                              then 'dep1'
+        when d.DEPARTMENT_ID >= 80  and d.DEPARTMENT_ID < 90    then 'dep2'
+        when d.DEPARTMENT_ID >= 90  and d.DEPARTMENT_ID < 100   then CASE
+                                                                        when e.SALARY < 5000 then 'smile'
+                                                                        else                      'slile x2'
+                                                                     END
+                                                                                             else ':)'
+    END                                                                 
+                                                                        departments_smile, 
+    DECODE (JOB_ID,    'AD_PRES',       'blue', 
+                       'AD_VP',         'red',
+                       'IT_PROG',       'green', 
+                       'FI_ACCOUNT',    'white',
+                                        'other_color')  "COLORS"
+    FROM
+        employees   e,
+        departments d
+    WHERE
+        e.department_id = d.department_id
+*/
+
+
