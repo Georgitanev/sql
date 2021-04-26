649b0b9c95e7a7e7a8517151396c572e0d1e3702
Georgi Tanev <georgi.tanev86@gmail.com>
2021-04-26 10:47:09 +0300

    sql files are better formatted

---
+++ sql_zoo/self_join.sql
@@ -0,0 +1,11 @@
+﻿--1.How many stops are in the database.
+
+select count(id) from stops
+
+--2.Find the id value for the stop 'Craiglockhart'
+
+select 	id 
+from 	stops
+where 	name= 'Craiglockhart'
+
+
