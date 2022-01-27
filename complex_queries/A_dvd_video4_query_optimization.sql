42232023964a51983bb732037c7f7d8bf81ff380
Georgi Tanev <georgi.tanev86@gmail.com>
2022-01-27 15:02:45 +0200

    dvd rental - query optimization

---
+++ complex_queries/A_dvd_video4_query_optimization.sql
@@ -0,0 +1,33 @@
+﻿-- Query optimization
+
+--select columns by their names not * 
+--limit results to what you really need.
+
+
+explain analyze
+	
+select * from customer --0.146 ----------------------3.394 -- with order by 0.489 -- 0.149 -- 599 rows 
+						--Execution Time: 0.275 ms
+order by 
+	last_name -- 1.357
+	
+	
+limit 10
+
+select 
+customer_id,
+store_id,
+last_name,
+first_name,
+email,
+address_id,
+activebool,
+create_date,
+last_update,
+active
+from customer   ---0.224 -- 0.017
+--order by 
+--	last_name -- 1.531
+limit 10
+
+
